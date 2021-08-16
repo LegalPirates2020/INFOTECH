08f30bb4e8222a23996bb459a6525f997cf2dbe7
LegalPirates2020 <legalpirates2020@gmail.com>
2021-08-16 15:51:58 +0530

    changes

--- courses/course.js
+++ courses/course.js
@@ -26,26 +26,60 @@ mobile(flutterma,flutter)
 mobile(dartma,dart)
 
 
+
+
+// img3.addEventListener('mouseenter',()=>{
+    //     side.classList.add('frontside')
+// })
+
+// img3.addEventListener('mouseleave',()=>{
+//     side.classList.remove('frontside')
+// })
+
+// img4.addEventListener('mouseenter',()=>{
+    //     side.classList.remove('backside')
+    // })
+    
+    // img4.addEventListener('mouseleave',()=>{
+        //     side.classList.remove('backside')
+        // })
+
+
 const mainside = document.querySelector(".mainside")
 const firstside = document.querySelector(".firstside")
 const secondside = document.querySelector(".secondside")
-const side=document.querySelector('.side')
+const side = document.querySelector('.side')
+const side1 = document.querySelector('.side1')
 const img3 = document.querySelector(".img3")
 const img4 = document.querySelector(".img4")
 
-
-img3.addEventListener('mouseenter',()=>{
-    side.classList.add('frontside')
+img3.addEventListener("mouseenter", () =>{
+    firstside.classList.add('frontside')
+})
+img3.addEventListener("mouseleave", () =>{
+    firstside.classList.remove('frontside')
 })
 
-img3.addEventListener('mouseleave',()=>{
-    side.classList.remove('frontside')
+img4.addEventListener("mouseenter", () => {
+    secondside.classList.add('frontside')
+})
+img4.addEventListener("mouseleave", () => {
+    secondside.classList.remove('frontside')
 })
 
-img4.addEventListener('mouseenter',()=>{
-    side.classList.remove('backside')
+img3.addEventListener("mouseenter", () =>{
+    mainside.style.opacity = "0"
+    mainside.classList.add('backside')
 })
-
-img4.addEventListener('mouseleave',()=>{
-    side.classList.remove('backside')
+img3.addEventListener("mouseleave", () =>{
+    mainside.style.opacity = "1"
+    mainside.classList.remove('backside')
+})
+img4.addEventListener("mouseenter", () =>{
+    mainside.style.opacity = "0"
+    mainside.classList.add('backside')
+})
+img4.addEventListener("mouseleave", () =>{
+    mainside.style.opacity = "1"
+    mainside.classList.remove('backside')
 })
