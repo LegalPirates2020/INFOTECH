4709c953340382a1644501a549e0258c54d6051f
LegalPirates2020 <legalpirates2020@gmail.com>
2021-08-17 04:46:05 -0700

    prasana gay

--- faculty/faculty.js
+++ faculty/faculty.js
@@ -1,13 +1,25 @@
 
 const cardContent = document.querySelectorAll('.container-2');
-const scrollContent = document.querySelectorAll('.scroll-content')
+const scrollContent = document.querySelectorAll('.scroll-content');
+const box1 = document.querySelector('.box1');
+const box2 = document.querySelector('.box2');
+const box3 = document.querySelector('.box3');
+const box4 = document.querySelector('.box4');
+const box5 = document.querySelector('.box5');
+const box6 = document.querySelector('.box6');
+
 scrollContent.forEach(element => {
   element.addEventListener('click', function content() {
     // scrollContent.forEach(content => content.classList.remove('animation'));
     cardContent.forEach(content => content.classList.remove('show'));
-    console.log(this)
-    scrollContent.forEach(btn => btn.classList.remove('color'))
+    scrollContent.forEach(content => content.classList.remove('color'));
+    console.log(box1.classList)
     this.classList.add('color');
+  
+    
+
+    
+    
     const carContentItems = document.querySelector(`.${this.id}-content`);
     carContentItems.classList.add('show');
   })
